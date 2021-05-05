09e4f959ca00f75cce0f215525f491ec948a7fc9
GoranL <45197404+chili94@users.noreply.github.com>
2021-05-05 04:52:37 -0700

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -3,4 +3,6 @@ sudo yum update
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+#echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+wget -O /var/www/html/index.html https://basichtml344.s3.us-east-2.amazonaws.com/html.txt
+chown -R apache. /var/www/html
